19b3efa6a04d92cf36efe38556aa53814015f436
venkat prasad <prasadsgit@gmail.com>
2016-10-25 02:47:58 -0700

    Initial commit

---
+++ sample.c
@@ -0,0 +1,17 @@
+Type: Internal
+Titile: Urgent action Needed: QUICLOST-136717,QUICLOST-136933
+
+Description:
+
+DO NOT enter any comments in PRISM. Please call the reporter and directly discuss any questions, any objections, etc…
+DO NOT withdraw this CR, or mark it DUPLICATE, or Needs Release etc…
+DO NOT edit the Release Notes
+But DO propagate the changes as needed
+
+Software Image Record:
+LA.BR.1.3.3-01801-8952.1-1
+
+Blacklisting CR:
+1501811
+
+
